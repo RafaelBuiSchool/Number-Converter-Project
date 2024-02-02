11e546dba44db8e90af5fbd2dad05ab7161ccb36
RafaelBuiSchool <rafaelb54@nycstudents.net>
2024-02-02 08:10:40 -0500

    Finished

--- ConverterRunner.java
+++ ConverterRunner.java
@@ -31,45 +31,68 @@ class ConverterRunner {
             int base = Integer.parseInt(secondChoice);
             System.out.print("Enter your number: ");
             String number = s.nextLine();
-            int n = Integer.parseInt(number);
-            s.close();
-            nc = new NumberConverter(n, base);
-            if (nc != null && !nc.isError()) {
-                int powerOf = 0;
-                int value = 0;
-                for (int i = nc.digits.length - 1; i >= 0; i--) {
-                    value += Math.pow(base, powerOf) * nc.digits[i];
-                    powerOf++;
+            number = number.toLowerCase();
+            if(!number.contains("0") || !number.contains("1") ||!number.contains("2") ||!number.contains("3") ||!number.contains("4") ||!number.contains("5") ||!number.contains("6") ||!number.contains("7") ||!number.contains("8") ||!number.contains("9")){
+                if(base == 2){
+                    System.out.println("Error Message: Number entered is not of base 2.");
                 }
-                String hexadecimal = Integer.toHexString(value).toUpperCase();
-                int[] digits = nc.getDigits();
-                if (nc.base == 2) {
-                    System.out.println("\nDecimal Number: " + value);
-                    System.out.println("Octal Number: " + Integer.toOctalString(value));
-                    System.out.println("Hexadecimal Number: " + hexadecimal);
-                } else if (nc.base == 8) {
-                    System.out.println("\nDecimal Number: " + value);
-                    System.out.println("Binary Number: " + Integer.toBinaryString(value));
-                    System.out.println("Hexadecimal Number: " + hexadecimal);
-                } else if (nc.base == 10) {
-                    System.out.println("\nBinary Number: " + Integer.toBinaryString(value));
-                    System.out.println("Octal Number: " + Integer.toOctalString(value));
-                    System.out.println("Hexadecimal Number: " + hexadecimal);
-                } else if (nc.base == 16) {
-                    System.out.println("\nBinary Number: " + Integer.toBinaryString(value));
-                    System.out.println("Octal Number: " + Integer.toOctalString(value));
-                    System.out.println("\nDecimal Number: " + value);
+                if(base == 8){
+                    System.out.println("Error Message: Number entered is not of base 8.");
+                }
+                if(base == 10){
+                    System.out.println("Error Message: Number entered is not of base 10.");
+                }
+                else if(base==16 && (!number.contains("a") || !number.contains("b") ||!number.contains("c") ||!number.contains("d") ||!number.contains("e") ||!number.contains("f"))){
+                    System.out.println("Error Message: Number entered is not of base 16.");
                 }
             }
+            else{
+                int n = Integer.parseInt(number);
+                s.close();
+                nc = new NumberConverter(n, base);
+                if (nc != null && !nc.isError()) {
+                    int powerOf = 0;
+                    int value = 0;
+                    for (int i = nc.digits.length - 1; i >= 0; i--) {
+                        value += Math.pow(base, powerOf) * nc.digits[i];
+                        powerOf++;
+                    }
+                    String hexadecimal = Integer.toHexString(value).toUpperCase();
+                    int[] digits = nc.getDigits();
+                    if (nc.base == 2) {
+                        System.out.println("\nDecimal Number: " + value);
+                        System.out.println("Octal Number: " + Integer.toOctalString(value));
+                        System.out.println("Hexadecimal Number: " + hexadecimal);
+                    } else if (nc.base == 8) {
+                        System.out.println("\nDecimal Number: " + value);
+                        System.out.println("Binary Number: " + Integer.toBinaryString(value));
+                        System.out.println("Hexadecimal Number: " + hexadecimal);
+                    } else if (nc.base == 10) {
+                        System.out.println("\nBinary Number: " + Integer.toBinaryString(value));
+                        System.out.println("Octal Number: " + Integer.toOctalString(value));
+                        System.out.println("Hexadecimal Number: " + hexadecimal);
+                    } else if (nc.base == 16) {
+                        System.out.println("\nBinary Number: " + Integer.toBinaryString(value));
+                        System.out.println("Octal Number: " + Integer.toOctalString(value));
+                        System.out.println("\nDecimal Number: " + value);
+                    }
+                }
+            }
+
         }
     }
     private static String newBaseValues(int value) {
+         String[] lowerAlphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+         String[] upperAlphabet={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
         if (value >= 0 && value <= 9) {
-            return (String) ("0" + value);
+            return (String) (value + "");
         } else if (value >= 10 && value <= 35) {
-            return (String) ("A" + (value - 10));
+            int i = value-10;
+            return (lowerAlphabet[i] + "");
         } else if (value >= 36 && value <= 61) {
-            return (String) ("a" + (value - 36));
+            int i = value-36;
+            return (upperAlphabet[i] + "");
         } else if (value == 62) {
             return "+";
         } else if (value == 63) {
